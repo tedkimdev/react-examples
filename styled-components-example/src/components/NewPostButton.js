5e1eab6e38c351c13450b9dc96a0edca399f6b0f
Ted Kim <yenafirst91@gmail.com>
2019-01-06 16:38:54 -0500

    Complete inheritance and composition

--- styled-components-example/src/components/NewPostButton.js
+++ styled-components-example/src/components/NewPostButton.js
@@ -3,18 +3,44 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { FaPlus as PlusIcon } from 'react-icons/fa/';
 
-// TODO: use withComponent to inherit Button component styles
-const NewPostLink = styled(Link)`
+import Button from './Button';
+
+import { CIRCLE, FLEX } from '../style/mixins';
+import THEME from '../style/theme';
+
+const NewPostLink = styled(Button)`
+  ${FLEX}
+  ${CIRCLE(48)}
+
+  background-color: ${THEME.blue.base};
+  color: white;
+  transition: 175ms ease-in-out;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.25);
+
+  :hover {
+    background-color: ${THEME.blue.darker};
+    transform: translateY(-2px);
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.4);
+  }
+
+  :active {
+    background-color: ${THEME.blue.darkest};
+    transform: translateY(0px);
+  }
+
+  :focus {
+    box-shadow: 0 0 4px ${THEME.blue.base};
+  }
+
   position: fixed;
   bottom: 12px;
   right: 12px;
-`;
+`.withComponent(Link);
 
 export default function NewPostButton() {
   return (
     <NewPostLink to="/new">
       <PlusIcon size={16} />
-      New post
     </NewPostLink>
   );
 }
