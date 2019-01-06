b4a243848f574fef604fb98021662eb483b5d38a
Ted Kim <yenafirst91@gmail.com>
2019-01-06 16:01:26 -0500

    Add traditional css example

---
+++ styled-components-example/src/components/NewPostButton.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { FaPlus as PlusIcon } from 'react-icons/fa/';
+
+const NewPostLink = styled(Link)`
+  position: fixed;
+  bottom: 12px;
+  right: 12px;
+`;
+
+export default function NewPostButton() {
+  return (
+    <NewPostLink to="/new">
+      <PlusIcon size={16} />
+      New post
+    </NewPostLink>
+  );
+}
